2505b51ac51db1de562f588be47938ac63655217
HARSH825 <harshchhallani3@gmail.com>
2025-02-01 19:10:10 +0530

    add currentLoans and filetering on params

--- backend/routes/lenderRouter.js
+++ backend/routes/lenderRouter.js
@@ -6,10 +6,13 @@ const router = Router();
 import LendersignupPhoneController from '../controller/lenderSignupPhoneController.js';
 import LendersignupEmailController from '../controller/lenderEmailController.js';
 import LendersignupPasswordController from '../controller/lenderPasswordController.js';
+import getLiveLoansController from "../controller/getLiveLoansController.js";
 
 // Lender Routes
 router.post('/signup/phoneNumber', LendersignupPhoneController);
 router.post('/signup/email', LendersignupEmailController);
 router.post('/signup/password', LendersignupPasswordController);
 // router.post('/login',LenderloginController);
+router.get('/liveloans', getLiveLoansController);  // add middleware remainig in this route to validate lender.
+
 export default router;
