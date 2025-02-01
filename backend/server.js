1b128efd97f2840a4027b468c8942eca8cd59dbf
Achintya Chavan <achintya.chavan22@spit.ac.in>
2025-02-01 13:48:49 +0530

    add dbConnection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors'; 
 import dotenv from 'dotenv'
+import connectDb from './db.js/connectDb.js';
 dotenv.config();
 
 const app = express(); 
  app.use(cors()); 
  app.use(express.json());  
-//  connectDb();  
+ connectDb();  
  
 const PORT = process.env.PORT;
 
