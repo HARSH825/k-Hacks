419f54031d30d4a69409ef9c70f8376596bae6d4
HARSH825 <harshchhallani3@gmail.com>
2025-02-01 19:11:12 +0530

    mongo_uri temproary solution

--- backend/db.js/connectDb.js
+++ backend/db.js/connectDb.js
@@ -2,11 +2,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://harshchhallani3:teamQuisk@cluster0.lkhvc.mongodb.net/kaggle";
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-       
+        const conn = await mongoose.connect(MONGO_URI, {
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
